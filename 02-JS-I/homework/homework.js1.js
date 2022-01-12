5b746baec5ee8ae8e64cd7a1631d8086a2f47a9b
Diiegote <diego.elprado22@gmail.com>
2022-01-12 16:01:29 -0300

    tarea

---
+++ 02-JS-I/homework/homework.js1.js
@@ -0,0 +1,7 @@
+Variables (una variable se usa para almacenar el nombre de algo que usaremos mas tarde );
+Strings ( una "strings" es una cadena de texto que siempre van entre comillas );
+Funciones (argumentos, return):(una funcion es una porcion de codigo que hacen una tarea en concreto);
+(un argumento sirve para darle nombre a una funcion y asi poder invocarla);
+(el retorn es para que nos devuelva un valor y finalice una funcion);
+Declaraciones if(un if se utiliza para evialuar una exprecion condicional, si se cumple devuelve verdadero y si es falsa ejecuta otra sentencia, en este caso seria false);
+Valores booleanos (true, false)(los valores booleanos son tipo de dato logico que representa logica binaria y devuelve solamente true(verdadero) o false (falso));
