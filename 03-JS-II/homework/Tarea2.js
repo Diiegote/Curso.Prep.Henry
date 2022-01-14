16175fcf02abac968ce5e91ba0b075d5c16c0278
Diiegote <diego.elprado22@gmail.com>
2022-01-14 16:21:09 -0300

    Tarea2.js

---
+++ 03-JS-II/homework/Tarea2.js
@@ -0,0 +1,4 @@
+for (un for se ejecuta las veces que nosotros le indiquemos) ;
+&& (significa and y solo acepta valores booleanos,and devuelve verdadero si los dos son verdadero, si hay algun valor falso devuelve falso) ;
+|| (significa or y solo acepta valores booleanos, or devuelve falso si los dos son falsos , si hay alguno verdadero devuelve verdadero) ;
+! (significa not y lo que hace es cambiar el valor que devuelve, si devuelve verdadero lo cambia a falso) ; 
