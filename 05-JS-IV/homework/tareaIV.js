41d4d8693eeccab654384b5555dfdbb77a85d6a1
Diiegote <diego.elprado22@gmail.com>
2022-01-18 20:59:48 -0300

    tareaIV

---
+++ 05-JS-IV/homework/tareaIV.js
@@ -0,0 +1,9 @@
+const { DATA_CLONE_ERR } = require("domexception")
+
+const { cornflowerblue } = require("color-name")
+
+    * Objetos:un objeto es una coleccion de propiedades , lo puede ver como una taza(la taza seria el objeto)  que tiene propiedades (el color , los dibujos, y el liquido que lleve dentro , serian las propiedades) ;
+	* Propiedades: las propiedades son los nombres que llevan los objetos y pueden ser como variables y acepta cualquier tipo de dato , ya sea strings o metodos ; 
+	* Métodos : los metodos son funciones que podemos invocar usando () ;
+	* Bucle `for…in` : los bucles se utilizan para hacer tareas repetitivas en base a una condicion . el bucle for se utiliza de manera que sabemos las veces que queremos que se repita la accion.
+	* Notación de puntos vs notación de corchetes : bracket notation cumple la misma funcion que el dot notation , la unica diferencia es que si queremos acceder a una propiedad del objeto con una variable si o si tenemos que usar el bracket notation.
